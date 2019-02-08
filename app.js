77d656a8f5df74b76c39e0e90fd19edf395a0bde
Fronczyk Magdalena <fronczyk@prsa.pl>
2019-02-08 14:54:06 +0100

    adds validation js

--- app.js
+++ app.js
@@ -4,17 +4,39 @@ document.addEventListener("DOMContentLoaded", () => {
   const formData = new FormData();
   let extraFieldsvalidation;
   let textInputvalidation;
+  let agreementValidation;
+  let emailValidation;
 
   const handleTextInput = (selector, key) => {
 
     const input = document.querySelector(selector);
     const value = input.value;
 
-    if (value === "" || value.length < 3 || value.length > 50 || isNaN(value) === false) {
+
+    if (value.length < 0 || value.length < 3 || value.length > 50 || isNaN(value) === false) {
       input.classList.remove("valid");
       input.classList.add("invalid");
+
+      if (value.length <= 0) {
+        const error = document.querySelector(".error-message.empty");
+        error.style.display = "block";
+      } else if (value.length >= 1 && value.length < 3) {
+        const error = document.querySelector(".error-message.short");
+        error.style.display = "block";
+      } else if (value.length > 50) {
+        const error = document.querySelector(".error-message.long");
+        error.style.display = "block";
+      }
+
       textInputvalidation = false;
+
     } else {
+      const errors = document.querySelectorAll(".error-message");
+
+      errors.forEach(error => {
+        error.style.display = "none"
+      });
+
       input.classList.remove("invalid");
       input.classList.add("valid");
       formData.append(key, value);
@@ -24,11 +46,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const handleCheckbox = (selector) => {
     const checkedBoolean = document.querySelector(selector).checked;
+    if (!checkedBoolean) {
+      agreementValidation = false;
+    } else {
+      agreementValidation = true;
+    }
     const rulesJson = JSON.stringify({
       IsAcceptedGeneralRules: checkedBoolean
     });
     formData.append("rulesJson", rulesJson);
   }
+
+
 
   // const handleFiles = (selector, name) => {
   //   const files = document.querySelector(selector).files;
@@ -100,8 +129,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const extraFieldsJson = JSON.stringify(extraFields);
 
-    formData.append("ExtraFieldsJSON", extraFieldsJson);
+    console.log(extraFieldsJson);
+    console.log(extraFields)
 
+    formData.append("ExtraFieldsJSON", extraFields);
+
+  }
+
+  const validateEmail = (email) => {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  const handleEmail = (selector, key) => {
+    const emailInput = document.querySelector(selector);
+    const emailValue = emailInput.value;
+
+    if (validateEmail(emailValue)) {
+      emailInput.classList.remove("invalid");
+      emailInput.classList.add("valid");
+      emailValidation = true;
+      formData.append(key, emailValue);
+    } else {
+      emailValidation = false;
+      emailInput.classList.remove("valid");
+      emailInput.classList.add("invalid");
+    }
   }
 
   formButton.addEventListener("click", e => {
@@ -109,7 +162,8 @@ document.addEventListener("DOMContentLoaded", () => {
     handleTextInput("#firstName", "FirstName");
     handleTextInput("#lastName", "LastName");
     handleTextInput("#nick", "Nick");
-    // handleInputText("#e-mail", "Email");
+    handleEmail("#e-mail", "Email");
+
     handleCheckbox("#agreement");
     // handleFiles("#music-file", "audio-file");
     // handleFiles("#image-file", "image-file");
@@ -121,7 +175,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     e.preventDefault();
 
-    if (textInputvalidation && extraFieldsvalidation) {
+    if (textInputvalidation && extraFieldsvalidation && agreementValidation && emailValidation) {
       axios({
         method: 'post',
         data: formData,
